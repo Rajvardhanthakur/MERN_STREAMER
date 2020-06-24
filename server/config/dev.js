996338b8d02d22c256712e17bd01222a9539e285
Rajvardhan <rajthakur3619@gmail.com>
2020-06-24 12:49:45 +0530

    moved connection to config

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://Raj:raj12345@cluster0-wzok5.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const config = require('./config/key');
+
 const { User } = require('./models/user');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://Raj:raj12345@cluster0-wzok5.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('DB Connected')).catch(err => console.error(err));
+mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('DB Connected')).catch(err => console.error(err));
 
 app.use(bodyParser.urlencoded({ extended : true}));
 app.use(bodyParser.json())
